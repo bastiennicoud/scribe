19d87b7e800d1294d4d1876f9bee0655dbf00c63
Bastien <bastien.nicoud@gmail.com>
2018-05-08 08:00:57 +0200

    Add logos

---
+++ src/js/background.js
@@ -0,0 +1,11 @@
+/**
+ * Background
+ * 
+ * Listeners for the extension, executed as backgroud script by chrome
+ * @author Bastien Nicoud
+ */
+
+// Listen to the click on the extension button in the chrome toolbar
+chrome.browserAction.onClicked.addListener(function () {
+  console.log('Toto')
+})
